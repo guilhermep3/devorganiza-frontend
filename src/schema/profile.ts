1a4b17a25cfe7c1045c3673de2eaa776d579284e
Guilherme <145510603+Guilhermep7@users.noreply.github.com>
2025-12-17 21:40:17 -0300

    fix profileImage schema

--- src/schema/profile.ts
+++ src/schema/profile.ts
@@ -4,7 +4,7 @@ export const editProfileSchema = z.object({
   name: z.string().min(2, "O nome é obrigatório"),
   username: z.string().min(3, "O usuário é obrigatório"),
   password: z.string().min(4, "Senha é obrigatória").optional().or(z.literal("")),
-  rofileImage: typeof window === 'undefined' 
+  profileImage: typeof window === 'undefined' 
     ? z.any().optional() 
     : z.instanceof(FileList).optional(),
 });
