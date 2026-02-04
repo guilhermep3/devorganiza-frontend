d12c15b9d314c831f0a1008fdd42b7234f12c79e
Guilherme <145510603+Guilhermep7@users.noreply.github.com>
2026-02-04 20:58:05 -0300

    fix: border and shadow in services card

--- components/servicesCard.tsx
+++ components/servicesCard.tsx
@@ -14,8 +14,8 @@ export const ServicesCard = ({ data, Icon }: props) => {
       whileInView={{ scale: 1 }}
       transition={{ delay: 0.1, duration: 0.8, ease: "easeInOut", }}
     >
-      <div className="group flex flex-col gap-3 p-6 rounded-lg transition border border-zinc-100 hover:border-gray-20
-        hover:shadow-md hover:shadow-gray-30 dark:hover:shadow-black"
+      <div className="group flex flex-col gap-3 p-6 rounded-lg transition border border-gray-10 hover:border-gray-20
+        hover:shadow-md hover:shadow-gray-30 dark:hover:shadow-gray-10"
       >
         <div className="flex items-center gap-5">
           <div className="p-3 bg-main-20/50 group-hover:bg-main-20 group-hover:scale-110 group-hover:rotate-12
