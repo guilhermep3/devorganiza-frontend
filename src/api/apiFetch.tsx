e64b03aff5017ff41295f66f23bc474f92f3f7ca
Guilherme <145510603+Guilhermep7@users.noreply.github.com>
2026-04-12 13:15:28 -0300

    feat: include authorization bearer token in apifetch

--- src/api/apiFetch.tsx
+++ src/api/apiFetch.tsx
@@ -3,10 +3,13 @@ const API_URL = process.env.NEXT_PUBLIC_API_URL!;
 type FetchOptions = Omit<RequestInit, "credentials">;
 
 export async function apiFetch(path: string, options: FetchOptions = {}) {
+  const token = document.cookie.split('; ').find(row => row.startsWith('token='))?.split('=')[1]
+
   const res = await fetch(`${API_URL}${path}`, {
     ...options,
     credentials: "include",
     headers: {
+      ...(token && { Authorization: `Bearer ${token}` }),
       ...(options.body && !(options.body instanceof FormData)
         ? { "Content-Type": "application/json" }
         : {}),
